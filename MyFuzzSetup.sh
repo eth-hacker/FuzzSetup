8cfcd82111abb91148b1cbdd35b2dcda8bbfecec
eth-hacker <105667596+eth-hacker@users.noreply.github.com>
2022-11-24 10:42:29 +0300

    Update MyFuzzSetup.sh

--- MyFuzzSetup.sh
+++ MyFuzzSetup.sh
@@ -2,7 +2,7 @@
 
 pip install slither-analyzer
 pip install solc-select
-wget https://github.com/crytic/echidna/releases/download/v2.0.3/echidna-test-2.0.3-Ubuntu-18.04.tar.gz
+wget https://github.com/crytic/echidna/releases/download/v2.0.4/echidna-test-2.0.4-Ubuntu-18.04.tar.gz
 gunzip echidna-test-2.0.3-Ubuntu-18.04.tar.gz
 tar -xf echidna-test-2.0.3-Ubuntu-18.04.tar
 curl -L https://foundry.paradigm.xyz | bash
