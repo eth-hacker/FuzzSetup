3ea5e19769e0be5d54d4b39124140c031056a4d7
eth-hacker <105667596+eth-hacker@users.noreply.github.com>
2022-11-24 10:51:08 +0300

    Update MyFuzzSetup.sh

--- MyFuzzSetup.sh
+++ MyFuzzSetup.sh
@@ -7,3 +7,6 @@ gunzip echidna-test-2.0.4-Ubuntu-18.04.tar.gz
 tar -xf echidna-test-2.0.4-Ubuntu-18.04.tar
 curl -L https://foundry.paradigm.xyz | bash
 rm echidna-test-2.0.4-Ubuntu-18.04.tar
+sudo apt-get update && sudo apt-get install python3 python3-pip -y
+pip3 install --user etheno
+npm -g i ganache
